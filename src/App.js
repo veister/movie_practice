63b82a35809a2944261a0283d613a354bdca98db
veister <rhkd2060@naver.com>
2021-02-24 17:42:05 +0900

    movie info get url cool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
 
-  getMovies = () => {
-    const movie = axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
     );
+
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "Ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                runtime={movie.runtime}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({ id, year, runtime, title, summary, poster }) {
+  return <h5>{title}</h5>;
+}
+
+Movie.prototype = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  runtime: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
